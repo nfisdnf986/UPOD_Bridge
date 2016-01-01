0010a0993dd6d60674784a5c76dcb2d6f425fd14
sunil <sunhick@gmail.com>
2016-01-01 00:08:55 -0700

    Included logging

--- Atheros/Processor.py
+++ Atheros/Processor.py
@@ -6,32 +6,48 @@ DataProcessor.py
 Get data from the queue and process it.
 """
 
+__author__ = "Sunil"
+__copyright__ = "Copyright 2015, Arduino UPOD Project"
+__license__ = "MIT License"
+__version__ = "0.0.0"
+__email__ = "suba5417@colorado.edu"
+
+
 import Queue
 import threading
 import logging
+import time
 
-global data_queue
+global queue
 
-BUFFSIZE = 100
-data_queue = Queue.Queue(BUFFSIZE)
+BUFFSIZE = 1000
+queue = Queue.Queue(BUFFSIZE)
 
-# log = logging.getLogger(__name__)
 
 class DataProcessor(threading.Thread):
     """
     Data processor
     """
     def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs=None, verbose=None):
+                 args=(), kwargs=None, verbose=None, event=None):
         super(DataProcessor,self).__init__()
         self.target = target
         self.name = name
-        return
+        self.setDaemon(True)
+        self.event = event
+        self.log = logging.getLogger(__name__)
 
     def run(self):
         # get data and process it
         while True:
-            data = data_queue.get()
-            # log.info(data)
-            print 'Data Processor --> ', data
-        return
+            # wait for the data to be put in queue
+            # if queue is empty. Otherwise process queue
+            if queue.empty():
+                self.event.wait()
+
+            data = queue.get()
+            self.process(data)
+
+    def process(self, data):
+        print 'Processing -->', data
+        self.log.info('Processing {data}'.format(data=data))
--- Atheros/Server.py
+++ Atheros/Server.py
@@ -6,6 +6,12 @@ server.py
 Read the data from the ATMega and post to processing Queue thread
 """
 
+__author__ = "Sunil"
+__copyright__ = "Copyright 2015, Arduino UPOD Project"
+__license__ = "MIT License"
+__version__ = "0.0.0"
+__email__ = "suba5417@colorado.edu"
+
 import sys
 import os
 
@@ -13,39 +19,68 @@ import os
 sys.path.insert(0, '/usr/lib/python2.7/bridge')  
 
 import logging
+import logging.config
+
 import Queue
 import time
+import signal
+import threading
 
-from Processor import DataProcessor, data_queue
-from threading import current_thread
+from Processor import DataProcessor, queue
 from bridgeclient import BridgeClient as bridgeclient
 
-# setup logging
 # set up the logging
-# print('Setting up logging module...')
-# basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
-# logging.config.fileConfig('%s/logging.conf' % basepath)
-# log = logging.getLogger(__name__)
+print('Setting up logging module...')
+basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
+logging.config.fileConfig('%s/logging.conf' % basepath)
+print basepath
 
+log = logging.getLogger(__name__)
+
+
+def signal_handler(signal, frame):
+    """ clean up data on Signal interrupt """
+    sys.exit(0)
 
 def main(*argv):
+    # install signal handler
+    signal.signal(signal.SIGINT, signal_handler)
+    log.info('Installed signal handler')
+
+    # setup event for thread synchronization
+    event = threading.Event()
+
     # setup data processing thread
-    processor = DataProcessor(name='DataProcessor-Thread')
+    processor = DataProcessor(name='DataProcessor-Thread', event=event)
     processor.start()
-    
+
+    # TODO: Explore BridgeClient code
+    # Performance: keep the socket open while subsequent get(...) operations
+    # By default BridgeClient open socket at begin of get(...) and closes after get(...)
+    # self.should_close_at_function_end = False
+
+    # get communication channel to ATMega
     channel = bridgeclient()
     
     last_seen = ''
     # Get data from the Tx-channel and post it the queue
     while True:
         data = channel.get('TX-channel')
-        if data == last_seen:
+
+        # Time stamp is a part of data contract between
+        # ATMega and Atheros. This makes sure that data
+        # is different from that of previous data
+
+        # Prevent duplicate data read on the bridgeclient before it's refeshed
+        if data is last_seen:
             continue
 
-        data_queue.put(data)
+        queue.put(data)
+        last_seen = data
+        event.set()
         time.sleep(.7)
 
 
 if __name__ == '__main__':
-    current_thread().name = 'Server-Thread'
+    threading.current_thread().name = 'Server-Thread'
     main(sys.argv)
