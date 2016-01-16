8d11e4c074a4f439894f70b5224b71ed256a5d4d
Sunil <sunhick@gmail.com>
2016-01-16 15:46:39 -0700

    use sshpass with password option. lazy to type!

--- Utils/cp2ar.sh
+++ Utils/cp2ar.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # copy the new python source files to the arduino
-scp /home/sunny/prv/github/UPOD_Bridge/Atheros/* root@arduino.local:/root/
+
+# sshpass to copy without entering the pasword
+# Remove sshpass if security is a concern
+sshpass -p 'arduino' scp /home/sunny/prv/github/UPOD_Bridge/Atheros/* root@arduino.local:/root/
 
 # copy GPS module sources
-scp /home/sunny/prv/github/UPOD_Bridge/Atheros/GPS/* root@arduino.local:/root/GPS/
+sshpass -p 'arduino' scp /home/sunny/prv/github/UPOD_Bridge/Atheros/GPS/* root@arduino.local:/root/GPS/
 
