9bb9350dfded70ae85bb6ba5d8b6a517e11d1945
Sunil <sunhick@gmail.com>
2016-01-23 02:05:52 -0700

    Added initial files

---
+++ Server/StreamingServer.py
@@ -0,0 +1,15 @@
+"""
+Streaming server
+
+Accept incoming data from the streaming clients,
+and store the data in Db
+
+"""
+
+class StreamingServer(object):
+    """ Streaming server class """
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
